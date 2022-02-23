b9bfa1ad4e0da18af66a8f10174e59d4525096fa
lee-android <jatyelsom@gmail.com>
2022-02-22 21:59:37 -0600

    added 'experiences' section.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-// import Experience from "./components/Experience";
+import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
@@ -129,10 +129,10 @@ class App extends Component {
           sharedSkills={this.state.sharedData.skills}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
-        {/* <Experience
+        <Experience
           resumeExperience={this.state.resumeData.experience}
           resumeBasicInfo={this.state.resumeData.basic_info}
-        /> */}
+        />
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
       </div>
     );
