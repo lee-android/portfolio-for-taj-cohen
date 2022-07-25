ba66a6365b9c5d654a82be6696eac247721e2477
lee-android <jatyelsom@gmail.com>
2022-07-25 05:49:17 -0500

    edited experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -36,7 +36,6 @@ class Experience extends Component {
               background: "#AE944F",
               color: "#fff",
               textAlign: "center",
-              fontSize: "3rem",
             }}
             icon={<i className="fab fa-html5 experience-icon"></i>}
             key={i}
